45acf6ad69d8934540af35077fbfcd843fd7b323
Divelix <divelix1@gmail.com>
2020-11-05 15:52:50 +0300

    Added main file for Raspberry Pi project

--- main.py
+++ main.py
@@ -1,38 +1,16 @@
-from omni import Car, vrep
-from input import OmniParser
-from vis import OmniPlotter, plt
-import sys
-import threading
+#!/usr/bin/env python3
+import serial
+import time
 
-vrep.simxFinish(-1)
-client_id = vrep.simxStart('127.0.0.1', 19997, True, True, 5000, 5)
+port = serial.Serial('/dev/ttyACM0', 115200)
+port.flush()
 
-if client_id != -1:
-    print('Connected to remote API server')
-    vrep.simxStartSimulation(client_id, vrep.simx_opmode_blocking)
-else:
-    sys.exit('Failed connecting to remote API server')
-
-# vrep.simxSynchronous(client_id, True) # call once
-# vrep.simxSynchronousTrigger(client_id) # each step
-# --------------------------------------------------------------
-car_name = 'Car'
-wheel_names = ['OmniTL', 'OmniTR', 'OmniBR', 'OmniBL']
-
-w = 0.125
-l = 0.125
-r = 0.09 / 2  # wheel radius
-car = Car(client_id, car_name, wheel_names, w, l, r)
-car_thread = threading.Thread(target=car.run)
-car_thread.start()
-
-parser = OmniParser(client_id, car)
-input_thread = threading.Thread(target=parser.run)
-input_thread.start()
-
-plotter = OmniPlotter(car)
-plt.show()
-
-car_thread.join()
-input_thread.join()
-
+while True:
+    # if port.in_waiting > 0:
+    start = time.time()
+    port.write(b"1.1 2.2 3.3 4.4\n")
+    line = port.readline().decode('utf-8').rstrip()
+    rpms = [float(rpm) for rpm in line.split(' ')]
+    print(f"sum = {sum(rpms)}| dt = {time.time() - start} secs")
+    # print(line)
+    # time.sleep(0.1)
---
+++ simMain.py
@@ -0,0 +1,38 @@
+from omni import Car, vrep
+from input import OmniParser
+from vis import OmniPlotter, plt
+import sys
+import threading
+
+vrep.simxFinish(-1)
+client_id = vrep.simxStart('127.0.0.1', 19997, True, True, 5000, 5)
+
+if client_id != -1:
+    print('Connected to remote API server')
+    vrep.simxStartSimulation(client_id, vrep.simx_opmode_blocking)
+else:
+    sys.exit('Failed connecting to remote API server')
+
+# vrep.simxSynchronous(client_id, True) # call once
+# vrep.simxSynchronousTrigger(client_id) # each step
+# --------------------------------------------------------------
+car_name = 'Car'
+wheel_names = ['OmniTL', 'OmniTR', 'OmniBR', 'OmniBL']
+
+w = 0.125
+l = 0.125
+r = 0.09 / 2  # wheel radius
+car = Car(client_id, car_name, wheel_names, w, l, r)
+car_thread = threading.Thread(target=car.run)
+car_thread.start()
+
+parser = OmniParser(client_id, car)
+input_thread = threading.Thread(target=parser.run)
+input_thread.start()
+
+plotter = OmniPlotter(car)
+plt.show()
+
+car_thread.join()
+input_thread.join()
+
